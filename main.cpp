9efaa641704acc87d18525df05bb02c82e99cacb
moweeece <mjespinosa990@gmail.com>
2024-10-01 18:33:42 -0700

    Initial Commit

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
+// COMSC-210 | Lab 10 | Mauricio Espinosa
+// IDE used: Visual Studio Code w/ Github
+
 #include <iostream>
 using namespace std;
 
 int main() {
 
     // add any code here, sort of a 'hello world plus' code block
+    cout << "Hello World" << endl;
 
     return 0;
 }
